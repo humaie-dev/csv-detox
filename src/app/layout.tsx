ddb9c8ee9f2f9f4e532adb982470a81338455984
The John kendall <john.kendall@humaie.com>
2026-02-04 15:58:20 +1100

    auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { ConvexReactClient } from "convex/react";
-import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import { Toaster } from "@/components/ui/toaster";
+import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
+import { ConvexReactClient } from "convex/react";
+
 import "./globals.css";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
@@ -12,10 +14,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <ConvexAuthProvider client={convex}>
-          {children}
+        <ConvexAuthNextjsServerProvider>
+          <ConvexAuthNextjsProvider client={convex}>{children}</ConvexAuthNextjsProvider>
           <Toaster />
-        </ConvexAuthProvider>
+        </ConvexAuthNextjsServerProvider>
       </body>
     </html>
   );
